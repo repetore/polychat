7044514a39daa200546b77b15aee9ddc60464929
repetor <stepan.koshkin2003@gmail.com>
2021-09-16 22:22:39 +0300

    server_of_polychat

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket, time
+
+
+host = socket.gethostbyname(socket.gethostname()) # 169.254.57.145
+port = 9090
+
+clients = []
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+
+running = True
+print(" ::[POLYCHAT STARTED]:: ")
+
+while running:
+	try:
+		data, addr = s.recvfrom(1024)
+
+		if addr not in clients:
+			clients.append(addr)
+
+
+		print(data.decode("utf-8"))
+
+		for client in clients:
+			if addr != client:
+				s.sendto(data,client)
+	except:
+		print("\n::[POLYCHAT STOPPED]::")
+		running = False
+s.close()
